c0b31987cfcf6dc940605744754af87ca0e4384d
Alid Castano <alidcastano@gmail.com>
2017-03-03 13:56:05 -0500

    configured more linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,13 +12,22 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    "brace-style": ["error", "stroustrup"],
+    "one-var": ["error", "always"],
     "space-before-function-paren": ["error", {
       "anonymous": "never",
       "named": "never",
       "asyncArrow": "always"
     }],
-    "one-var": ["error", "always"],
-    "indent": ["error", 2, { "VariableDeclarator": { "var": 2, "let": 2, "const": 3 } }]
+    "indent": ["error", 2,
+      {
+        "VariableDeclarator": {
+          "var": 2,
+          "let": 2,
+          "const": 3
+        }
+      }
+    ]
   },
   globals: {}
 }
