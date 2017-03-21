32e05085e44c4f9c92f56db552a526166ed05f50
Alid Castano <alidcastano@gmail.com>
2017-03-20 22:23:12 -0400

    removed task timeline from demo

--- demo/App.vue
+++ demo/App.vue
@@ -2,24 +2,13 @@
   <div id="app" class="component">
     <h1>{{pluginName}}</h1>
 
-    Test:
     <Test />
-
-    Enqueue:
-    <TaskTrackerGraph flow="enqueue" />
-
-    Restart:
-    <TaskTrackerGraph flow="restart" />
-
-    Drop:
-    <TaskTrackerGraph flow="drop" />
   </div>
 </template>
 
 
 <script>
 import Test from './Test'
-import TaskTrackerGraph from './TaskTrackerGraph'
 
 export default {
   data: () => ({
@@ -27,8 +16,7 @@ export default {
   }),
 
   components: {
-    Test,
-    TaskTrackerGraph
+    Test
   }
 }
 </script>
