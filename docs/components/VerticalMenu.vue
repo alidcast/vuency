503ec4d1f47117627c65338bbd157f7ebe7f7ca5
Alid Castano <alidcastano@gmail.com>
2017-03-22 16:02:33 -0400

    created vertical menu with nested links

---
+++ docs/components/VerticalMenu.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+  div.menu-container
+    ul(v-for="(group, index) in menu")
+      //- Figure out whether current group is a top level section (a string)
+      //- or a heading with subsections (another list).
+      template(v-if="isNested(group)")
+        li(v-for="section in group")
+          a.section-heading(:href="`/guide/${section}`")
+            | {{ section | titlelize }}
+      template(v-else)
+        h5.group-heading {{ group[0] | titlelize | capitalize }}
+        li(v-for="subsection in group[1]")
+          a.section-heading(:href="`/guide/${subsection}`") {{ subsection | titlelize }}
+</template>
+
+
+<script>
+import { toHeading } from '~utilities/to-transforms'
+
+export default {
+  props: {
+    menu: {
+      type: Array, required: true
+    }
+  },
+
+  filters: {
+    titlelize: toHeading,
+  
+    capitalize(str) {
+      return str.toUpperCase()
+    }
+  },
+
+  methods: {
+    isNested(list) {
+      return typeof list[1] === 'string'
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+.menu-container
+  padding: 1rem
+.group-heading
+  margin-bottom: .75rem
+  font-size: .9rem
+  font-weight: 500
+  color: #006400
+.section-heading
+  display: block
+  margin-bottom: .5rem
+</style>
--- docs/layouts/default.vue
+++ docs/layouts/default.vue
@@ -1,67 +1,43 @@
 <template lang="pug">
-  <div v-cloak>
+  div
     ThreeSlotSidebar.sidebar-content
       div.sidebar-header(slot="top")
         a.site-title(href="/") Vuency
         p.site-slogan Concurrency management <span> for Vuejs. </span>
-      ul.sidebar-menu(slot="middle")
-        template(v-for="(group, index) in menu")
-          li.heading.group {{ menu[index][0] | titlelize }}
-            li.heading.section(v-for="section in menu[index][1]")
-              a(:href="`/guide/${section}`") {{ section | titlelize }}
+
+      div(slot="middle")
+        VerticalMenu(:menu="menu")
+
       div.sidebar-links(slot="bottom")
         a.github-link(href="https://github.com/alidcastano/vuency")
           img#github-icon(src="~assets/img/Github-32px.png")
           span View on Github
-    nuxt.main(:class="{'app--hidden': isHidden }")
-  </div>
+    nuxt.main
 </template>
 
 <script>
 import config from '../docs.config'
-import { toHeading } from '~utilities/to-transforms'
 import ThreeSlotSidebar from '~components/ThreeSlotSidebar'
+import VerticalMenu from '~components/VerticalMenu'
 
 export default {
   data: () => ({
-    menu: config.menu,
-    isHidden: false
+    menu: config.menu
   }),
-  watch: {
-    $route: 'setStore'
-  },
-  methods: {
-    setStore() {
-      if (this.isHidden) this.isHidden = !this.isHidden
-    }
-  },
-  filters: {
-    titlelize: toHeading
-    // TODO capitalize
-  },
+
   components: {
-    ThreeSlotSidebar
+    ThreeSlotSidebar,
+    VerticalMenu
   }
 }
 </script>
 
-
-<style>
-[v-cloak] {
-  display: none;
-  background-color: blue;
-}
-</style>
-
 <style lang="sass">
 @import "../assets/sass/global.sass"
 
-.app
-  &--hidden
-    display: none
-
 .main
   margin-left: 18rem
+
 .sidebar-content
   padding-left: 1rem
   .sidebar-header
@@ -78,12 +54,6 @@ export default {
         display: block
         position: absolute
         right: 0
-  .sidebar-menu
-    padding: 1rem
-    .heading
-      margin-bottom: .75rem
-      &.group
-        color: #006400
   .sidebar-links
     .github-link
       width: 100%
