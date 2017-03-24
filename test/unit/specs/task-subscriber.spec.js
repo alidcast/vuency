26453bdff09a1f8c211dd4a377d7b5063d25492e
Alid Castano <alidcastano@gmail.com>
2017-03-24 15:57:33 -0400

    improved naming of subscriber methods

--- test/unit/specs/task-subscriber.spec.js
+++ test/unit/specs/task-subscriber.spec.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-expressions */
-/* global describe, it, expect */
+/* global describe, it, expect, beforeEach */
 
 import createTaskSubscriber from 'src/plugin/modifiers/task-subscriber'
 
@@ -14,13 +14,17 @@ describe('Task Subscribions', function() {
   it('has correct `before` subscriptions', async () => {
     expect(subs.beforeStart).to.not.be.undefined
     expect(subs.beforeNext).to.not.be.undefined
-
   })
+
   it('has correct `on` subscriptions', async () => {
     expect(subs.onCancel).to.not.be.undefined
     expect(subs.onDrop).to.not.be.undefined
     expect(subs.onRestart).to.not.be.undefined
     expect(subs.onError).to.not.be.undefined
     expect(subs.onSuccess).to.not.be.undefined
+  })
+
+  it('has correct `after` subscriptions', async () => {
+    expect(subs.afterEnd).to.not.be.undefined
   })
 })
