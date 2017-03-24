94955348105d19b1191c53b0aec6bd71ddd6ce14
Alid Castano <alidcastano@gmail.com>
2017-03-24 15:36:55 -0400

    added basic subscription tests

---
+++ test/unit/specs/task-subscriber.spec.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-expressions */
+/* global describe, it, expect */
+
+import createTaskSubscriber from 'src/plugin/modifiers/task-subscriber'
+
+
+describe('Task Subscribions', function() {
+  let subs
+  beforeEach(() => {
+    let subscriber = createTaskSubscriber()
+    subs = subscriber.subscriptions
+  })
+
+  it('has correct `before` subscriptions', async () => {
+    expect(subs.beforeStart).to.not.be.undefined
+    expect(subs.beforeNext).to.not.be.undefined
+
+  })
+  it('has correct `on` subscriptions', async () => {
+    expect(subs.onCancel).to.not.be.undefined
+    expect(subs.onDrop).to.not.be.undefined
+    expect(subs.onRestart).to.not.be.undefined
+    expect(subs.onError).to.not.be.undefined
+    expect(subs.onSuccess).to.not.be.undefined
+  })
+})
