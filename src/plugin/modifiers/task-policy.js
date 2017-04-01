e44ee40883fdacf3880e5ec65be1c7fd179cfe86
Alid Castano <alidcastano@gmail.com>
2017-03-31 20:08:15 -0400

    made maxRunning and delay flow options

--- src/plugin/modifiers/task-policy.js
+++ src/plugin/modifiers/task-policy.js
@@ -8,36 +8,27 @@ import assert from '../../util/assert'
 *  @constructs TaskPolicy
 */
 export default function createTaskPolicy(_type = 'default', _num = 1, _time = 0) {
-  let flowTypes = ['default', 'enqueue', 'restart', 'drop']
-
-  const currentPolicy = {
-    flow: _type,
-    delay: _time,
-    maxRunning: _num
-  }
+  let flowTypes = ['default', 'enqueue', 'restart', 'drop'],
+      currentPolicy = {
+        flow: _type,
+        delay: _time,
+        maxRunning: _num
+      }
 
   return {
-    // default scheduler policy
+    // default configuration
     get policy() {
       return currentPolicy
     },
 
     /**
-     *  Sets the scheduling rule for repeat calls and optionally the amount
-     *  of time to delay the scheduling of the task.
+     *  Sets the scheduling rule for repeat calls.
      */
-    flow(type, time = 0) {
+    flow(type, opts) {
       assert(flowTypes.indexOf(type) > -1, `${type} is not a flow control option`)
       currentPolicy.flow = type
-      currentPolicy.delay = time
-      return this
-    },
-
-    /**
-     * Sets the number of instances allowed to run concurrently.
-     */
-    maxRunning(num) {
-      currentPolicy.maxRunning = num
+      if (opts && opts.delay) currentPolicy.delay = opts.delay
+      if (opts && opts.maxRunning) currentPolicy.maxRunning = opts.maxRunning
       return this
     }
   }
